5d5e504726319424b00b66c87c499ecd103d6599
RFNoC <rfnoc@rfnocs-pro-51.lan>
2022-01-12 15:31:58 -0800

    simple middleware building - 3- respond with an error

--- api/server.js
+++ api/server.js
@@ -10,12 +10,15 @@ server.use((req, res, next) => {
   // here we can do whatever:
   // 1- respond to clients
   // 2- simply allow the request to flow to the next middleware
+  // 3- respond with an error
   console.log('the req flowed through our custom middleware')
   res.set('X-Web-49', 'Rocks') // setting a header on the response
   res.set('Set-Cookie', 'foo=bar') // setting a cookie on the browser
   // calling next without args allows the req,res,next to flow to the next middleware
   req.foo = 'bar'
-  next() // we never needed to call next because our endpoints were "the end of the line"
+  // next() // we never needed to call next because our endpoints were "the end of the line"
+
+  next({ status: 422, message: 'this is horrible'}) // sends this object to the closest error-handling middleware
 })
 
 server.use('/api/hubs', hubsRouter);
@@ -31,5 +34,12 @@ server.use('*', (req, res) => {
   // catch all 404 errors middleware
   res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
 });
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  console.log('disaster!')
+  res.status(err.status || 500).json({
+    message: `The Horror - ${err.message}`,
+  })
+})
 
 module.exports = server;
