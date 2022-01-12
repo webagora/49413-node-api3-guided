9e989e7bc8f356f711b735cb8fa05bc2b828dee1
Ladrillo <gabriel@cabrejas.net>
2022-01-12 19:08:52 +0100

    committing to "lecture"

--- api/hubs/hubs-middleware.js
+++ api/hubs/hubs-middleware.js
@@ -1,5 +1,9 @@
 function checkHubId(req, res, next) {
   // the req flows through
+  // hit the db with req.params.id
+  // make sure the id is "real"
+  // if it is, next()
+  // if it doesn't, next('dat no exist!)
 }
 
 
