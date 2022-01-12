fe1d867d601254126afeb1c292768309c8c65de6
Ladrillo <gabriel@cabrejas.net>
2022-01-12 19:28:09 +0100

    committing to "lecture"

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -16,6 +16,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:id', checkHubId, (req, res, next) => {
+  console.log('********', req.foo)
   Hubs.findById(req.params.id)
     .then(hub => {
       res.status(200).json(hub);
