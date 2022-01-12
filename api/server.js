5b49836415758087c6036e6715270a4371511810
Ladrillo <gabriel@cabrejas.net>
2022-01-12 18:34:32 +0100

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ server.use((req, res, next) => {
   // here we can do whatever:
   // 1- respond to clients
   // 2- simply allow the request to flow to the next middleware
-  // 3- 
+  // 3- respond with an error
   console.log('the req flowed through our custom middleware')
   res.set('X-Web-49', 'Rocks') // setting a header on the response
   res.set('Set-Cookie', 'foo=bar') // setting a cookie on the browser
