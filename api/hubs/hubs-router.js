c4fd2114d339d7c18efe91289e6bd3fcee18389f
Ladrillo <gabriel@cabrejas.net>
2022-01-12 18:55:41 +0100

    committing to "lecture"

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -11,11 +11,7 @@ router.get('/', (req, res, next) => {
       res.status(200).json(hubs);
     })
     .catch(error => {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error retrieving the hubs',
-      });
+      next(error)
     });
 });
 
