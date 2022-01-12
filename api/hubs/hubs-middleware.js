997032dc2d9fb996d7d8ee7ea3b9825e4dab4363
Ladrillo <gabriel@cabrejas.net>
2022-01-12 19:25:29 +0100

    committing to "lecture"

--- api/hubs/hubs-middleware.js
+++ api/hubs/hubs-middleware.js
@@ -1,11 +1,6 @@
 const Hub = require('./hubs-model')
 
 async function checkHubId(req, res, next) {
-  // the req flows through
-  // hit the db with req.params.id
-  // make sure the id is "real"
-  // if it is, next()
-  // if it doesn't, next('dat no exist!)
   try {
     const possibleHub = await Hub.findById(req.params.id)
     if (possibleHub) {
